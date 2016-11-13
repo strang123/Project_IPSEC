0214a9fd130ac9234e8fa9eb74aa72fba7874691
strang123 <jon.strang.strang@gmail.com>
2016-11-13 14:47:22 -0800

    now left and right gateways have their own configfunction

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ def main():
 	received_ip_array.append("2.1.1.1")
 	received_ip_array.append("3.1.1.1")
 	received_ip_array.append("4.1.1.1")
-	create_config_files(received_ip_array)
+	create_config_files_left(received_ip_array)
 
-def create_config_files(received_ip_array):
+def create_config_files_left(received_ip_array):
 	config_file = open('ipsec.conf','w')
 
 	config_file.write('conn test\n')	
@@ -31,6 +31,25 @@ def create_config_files(received_ip_array):
 	secret_file = open('ipsec.secrets','w')
 	
 	secret_file.write(received_ip_array[1] + ' ' + received_ip_array[2] + ' : PSK \"shared key\"\n')	
+
+
+def create_config_files_right(received_ip_array):
+	config_file = open('ipsec.conf','w')
+
+	config_file.write('conn test\n')	
+	config_file.write('\tleft=' + received_ip_array[2] + '\n')	
+	config_file.write('\tright=' + received_ip_array[1] + '\n')	
+	config_file.write('\tleftsubnet=' + received_ip_array[3] + '\n')	
+	config_file.write('\trightsubnet=' + received_ip_array[0] + '\n')	
+	config_file.write('\tleftfirewall=no\n')	
+	config_file.write('\trightfirewall=no\n')	
+	config_file.write('\tauto=start\n')	
+	config_file.write('\tauthby=secret\n')	
+	config_file.write('\ttype=tunnel')	
+
+	secret_file = open('ipsec.secrets','w')
+	
+	secret_file.write(received_ip_array[2] + ' ' + received_ip_array[1] + ' : PSK \"shared key\"\n')	
 
 def setup_a_socket_to_listen_to():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
