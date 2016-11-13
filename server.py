3e29dc9085d7dec716872565506279f5ad556cb5
strang123 <jon.strang.strang@gmail.com>
2016-11-13 12:42:36 -0800

    added for loop in server to send ip

--- server.py
+++ server.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python
-import socket, sys
+import socket, sys, os, subprocess
 #
 
 
 
 
 def main():
-        setup_a_socket_to_listen_to()
+        arr = setup_a_socket_to_listen_to()
+	for i in arr:
+		print arr[i]
+	#create_config_files()
+
+#def create_config_files()
+	
 
 def setup_a_socket_to_listen_to():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ('10.10.5.2', 10000)
+        server_address = ('10.10.5.1', 10000)
         sock.bind(server_address)
         sock.listen(1)
         listen_to_socket_and_receive_info(sock)
 
 def listen_to_socket_and_receive_info(sock):
+	ip_array = []
         while True:
                 connection, client_address = sock.accept()
                 try:
                         while True:
                                 data = connection.recv(16)
                                 if data:
-                                        connection.sendall(data)
-                                        print(data)
+                                        #connection.sendall(data)
+					ip_array.append(data)
+					if len(ip_array) == 4:
+						return ip_array
                                 else:   
                                         print >>sys.stderr, 'Connection with the following IP/Port pair has sent information', client_address
                                         break
