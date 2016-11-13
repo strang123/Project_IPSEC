3dd25b07034dde9fec2837f319f9b83f47217be3
strang123 <jon.strang.strang@gmail.com>
2016-11-13 13:03:54 -0800

    added workable client

--- jons_client.py
+++ jons_client.py
@@ -5,18 +5,25 @@ import socket, sys, getopt
 
 def main(argv):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = ('10.10.5.2',10000)
+	server_address = ('10.10.5.1',10000)
 	sock.connect(server_address)
 	try:
-		message = argv[0]
-		sock.sendall(message)
-		amount_received = 0
-		amount_expected = len(message)
+		message1 = argv[0]
+	#	message2 = argv[1]
+	#	message3 = argv[2]
+	#	message4 = argv[3]
+		sock.sendall(message1)
+		#sock.sendall(message2)
+		#sock.sendall(message3)
+		#sock.sendall(message4)
 		
-		while amount_received < amount_expected:
-			data = sock.recv(16)
-			amount_received += len(data)
-			print(data)
+		#amount_received = 0
+		#amount_expected = len(message)
+		#
+		#while amount_received < amount_expected:
+		#	data = sock.recv(16)
+		#	amount_received += len(data)
+		#	print(data)
 			
 	finally:
 		sock.close()
