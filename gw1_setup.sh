0226d1228333d85e749f536e409a259f43cb5fc6
strang123 <jon.strang.strang@gmail.com>
2016-11-19 14:43:59 -0800

    add arp

--- gw1_setup.sh
+++ gw1_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 iptables -F
 ufw disable
 
@@ -5,3 +7,6 @@ echo 1 >/proc/sys/net/ipv4/ip_forward
 
 ip route add 10.10.4.2/32 via 10.10.5.1 dev eth3
 ip route add 10.10.1.1/32 via 10.10.1.2 dev eth1
+
+arp -i eth1 -s 10.10.1.1 02:bc:14:94:03:12
+arp -i eth3 -s 10.10.5.2 02:58:91:db:74:19
