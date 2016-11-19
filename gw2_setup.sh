94a6bff5ccc785f17e0123c845ad16683dbe33fc
strang123 <jon.strang.strang@gmail.com>
2016-11-19 14:40:06 -0800

    added gw2

---
+++ gw2_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+iptables -F
+ufw disable
+
+echo 1 >/proc/sys/net/ipv4/ip_forward
+
+ip route add 10.10.1.1/32 via 10.10.5.2 dev eth3
+ip route add 10.10.4.2/32 via 10.10.4.1 dev eth2
