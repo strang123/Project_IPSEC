c344ebb26b81e192e668fe07f21d4c332d103530
strang123 <jon.strang.strang@gmail.com>
2016-11-19 15:01:04 -0800

    routes and arps done

--- gw1_setup.sh
+++ gw1_setup.sh
@@ -9,4 +9,4 @@ ip route add 10.10.4.2/32 via 10.10.5.1 dev eth3
 ip route add 10.10.1.1/32 via 10.10.1.2 dev eth1
 
 arp -i eth1 -s 10.10.1.1 02:bc:14:94:03:12
-arp -i eth3 -s 10.10.5.2 02:58:91:db:74:19
+arp -i eth3 -s 10.10.4.2 02:58:91:db:74:19
