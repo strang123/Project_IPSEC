ff6dc7a5f04ec98ca6b99e25d62f955d973fbb47
strang123 <jon.strang.strang@gmail.com>
2016-11-20 16:07:14 -0800

    changes some controller code

---
+++ controller.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import socket, sys, getopt
+
+GW1_ADDRESS='10.10.5.1'
+GW2_ADDRESS='10.10.5.2'
+
+
+def main(argv):
+	AddressA = argv[0]
+	AddressB = argv[1]
+	gateway1 = argv[2]
+	gateway2 = argv[3]
+	
+	message= AddressA+" "+AddressB+" "+gateway1+" "+gateway2
+	
+	sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	gateway1_address = (gateway1, 10000)
+	sock1.connect(gateway1_address)
+	
+	
+	sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	gateway2_address = (gateway2, 10000)
+	sock2.connect(gateway2_address)
+	
+	try:
+	
+		sock1.send(message)
+		sock2.send(message)
+	
+	
+	finally:
+		sock1.close()
+		sock2.close()
+	
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
