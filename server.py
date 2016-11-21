ee3392f982be744ae979dc8775540dae76da9dcb
strang123 <jon.strang.strang@gmail.com>
2016-11-20 16:23:42 -0800

    cleaned up

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket, sys, os, subprocess
 #
 
 PATH_TO_CONFS='/users/faezehka/strong_swan_install/conf'
-SERVER_ADDRESS='10.10.5.1'
+SERVER_ADDRESS='10.10.5.2'
 
 def main(argv):
 	while True:
